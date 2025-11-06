2523149a69c60d4bd8659b796b72a7b3a7ab393d
siddharth <163295466+chiku2312@users.noreply.github.com>
2025-11-06 15:56:50 +0530

    Update application.py

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "hogyi bhai deploy app benstalk se got it"
+    return "hogyi bhai deploy app benstalk se siddharth got it"
 
 @app.route('/about')
 def about():
